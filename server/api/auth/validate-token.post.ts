72a1eb0470ff70a8d0eabb0c42ed272565341cd4
Franco <franco_tigres18@hotmail.com>
2026-01-26 17:24:45 -0600

    feat(Auth):validate token api add

---
+++ server/api/auth/validate-token.post.ts
@@ -0,0 +1,23 @@
+import { eq } from "drizzle-orm";
+import { db } from "~~/server/db";
+import { usersTable } from "~~/server/db/schema";
+import { tokenSchema } from "~~/shared/schemas";
+
+export default eventHandler(async (event) => {
+  const body = await readBody(event);
+  const validatedData = tokenSchema.parse(body.token);
+
+  const user = await db
+    .select()
+    .from(usersTable)
+    .where(eq(usersTable.token, validatedData));
+
+  if (user.length === 0) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Token no válido",
+    });
+  }
+
+  return { message: "Token válido, asigna una nueva contraseña" };
+});
