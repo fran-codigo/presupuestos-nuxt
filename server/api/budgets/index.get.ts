8ad5eaa2827b6c8d1296f971db68cf0c790dd57b
Franco <franco_tigres18@hotmail.com>
2026-01-27 14:32:36 -0600

    feat(Budget):index budget api add

---
+++ server/api/budgets/index.get.ts
@@ -0,0 +1,15 @@
+import { desc, eq } from "drizzle-orm";
+import { db } from "~~/server/db";
+import { budgetsTable } from "~~/server/db/schema";
+
+export default eventHandler(async (event) => {
+  const { user } = await requireUserSession(event);
+
+const budgets = await db
+    .select()
+    .from(budgetsTable)
+    .where(eq(budgetsTable.userId, user.id))
+    .orderBy(desc(budgetsTable.id));
+
+  return { budgets };
+});
