7276059a682b04d2957c358bee76a69709857c34
Franco <franco_tigres18@hotmail.com>
2026-01-27 18:51:43 -0600

    feat(Expense):get budget with expenses

--- server/api/budgets/[id].get.ts
+++ server/api/budgets/[id].get.ts
@@ -1,8 +1,21 @@
+import { eq } from "drizzle-orm";
+import { db } from "~~/server/db";
+import { budgetsTable, expensesTable } from "~~/server/db/schema";
 import { validateBudgetOwnership } from "~~/server/utils/budget";
 
 export default eventHandler(async (event) => {
-
   const { budget } = await validateBudgetOwnership(event);
 
-  return { budget };
+  const results = await db
+    .select()
+    .from(budgetsTable)
+    .leftJoin(expensesTable, eq(budgetsTable.id, expensesTable.budgetId))
+    .where(eq(budgetsTable.id, budget!.id));
+
+  const formattedBudget = {
+    ...budget,
+    expenses: results.map((r) => r.expenses).filter((e) => e !== null),
+  };
+
+  return { budget: formattedBudget };
 });
