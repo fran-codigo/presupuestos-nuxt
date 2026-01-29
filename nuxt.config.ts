3513326a75978ff5262fd5c9355ea83bf7176a7c
Franco <franco_tigres18@hotmail.com>
2026-01-28 20:05:11 -0600

    feat:add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['nuxt-auth-utils']
-})
+  css: ["./app/assets/css/main.css"],
+  modules: ["nuxt-auth-utils"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
