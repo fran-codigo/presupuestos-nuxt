b0b6946689312fb386ef0405426074af8ccd609f
Franco <franco_tigres18@hotmail.com>
2026-01-29 21:43:12 -0600

    fix(Budget):improve handle errors at index post budget api

--- server/api/budgets/index.post.ts
+++ server/api/budgets/index.post.ts
@@ -3,13 +3,23 @@ import { budgetsTable } from "~~/server/db/schema";
 import { draftBudgetSchema } from "~~/shared/schemas";
 
 export default eventHandler(async (event) => {
-  const body = await readBody(event);
-  const validatedData = draftBudgetSchema.parse(body);
   const session = await requireUserSession(event);
+  const body = await readBody(event);
+  const validatedData = draftBudgetSchema.safeParse(body);
+
+  if (!validatedData.success) {
+    const errors = validatedData.error.issues.map((error) => error.message);
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Bad Request",
+      message: "Datos de presupuesto inválidos",
+      data: errors,
+    });
+  }
 
   await db.insert(budgetsTable).values({
-    name: validatedData.name,
-    amount: String(validatedData.amount),
+    name: validatedData.data.name,
+    amount: String(validatedData.data.amount),
     userId: session.user.id,
   });
 
